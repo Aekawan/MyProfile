f21412dd507da4bf546ae465480c520b554b791a
Aekawan <l3anksing123@gmail.com>
2019-09-27 23:18:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jsonfile = require('jsonfile')
 const FILE_PATH = './data.json'
 
 const makeCommit = n => {
-    if (n === 0) return simpleGit.push()
+    if (n === 0) return simpleGit().push()
     const x = random.int(0, 54)
     const y = random.int(0, 6)
     const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format()
@@ -16,7 +16,7 @@ const makeCommit = n => {
     }
     console.log(DATE)
     jsonfile.writeFile(FILE_PATH, data, () => {
-        simpleGit.add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n))
+        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n))
     })
 }
 
