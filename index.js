c381ed09e84bb5f5a051e69eb07ca6b4a92e18fd
Aekawan <l3anksingle123@gmail.com>
2019-09-28 00:36:10 +0700

    s

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const FILE_PATH = './data.json'
 
 const makeCommit = n => {
     if (n === 0) return simpleGit().push()
-    const x = random.int(0, 54)
+    const x = random.int(20, 54)
     const y = random.int(0, 6)
-    const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format()
+    const DATE = moment().subtract(1, 'm').add(1, 'd').add(x, 'w').add(y, 'd').format()
 
     const data = {
         date: DATE
@@ -20,4 +20,4 @@ const makeCommit = n => {
     })
 }
 
-makeCommit(5000)
+makeCommit(500)
